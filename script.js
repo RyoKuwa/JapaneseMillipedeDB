0d0deaa5c7d4290e1905835f350685743185a2fc
Ryosuke Kuwahara <80314792+RyoKuwa@users.noreply.github.com>
2025-02-22 23:02:03 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1244,3 +1244,7 @@ window.addEventListener("resize", adjustSearchContainer);
 
 // ページ読み込み時にも適用
 document.addEventListener("DOMContentLoaded", adjustSearchContainer);
+
+document.getElementById("search-all").addEventListener("focus", function() {
+  this.value = ""; // フィルタリングをリセット
+});
