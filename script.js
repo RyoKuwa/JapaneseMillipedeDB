dc403b761a2982003ff0f87baa7e2b008180c572
Ryosuke Kuwahara <80314792+RyoKuwa@users.noreply.github.com>
2025-02-22 23:49:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1263,9 +1263,9 @@ document.addEventListener("touchstart", function(event) {
   logDebug("タッチ開始: " + event.target.tagName);
 });
 
-document.getElementById("search-all").addEventListener("pointerdown", function(event) {
+document.getElementById("search-all").addEventListener("blur", function(event) {
   event.preventDefault();
   setTimeout(() => {
       this.focus();
-  }, 300);
+  }, 100);
 });
