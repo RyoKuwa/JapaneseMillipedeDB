ff98ee02ab5a22bfb2fcd714d70eb55398b1a3a5
Ryosuke Kuwahara <80314792+RyoKuwa@users.noreply.github.com>
2025-02-22 22:57:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1244,9 +1244,3 @@ window.addEventListener("resize", adjustSearchContainer);
 
 // ページ読み込み時にも適用
 document.addEventListener("DOMContentLoaded", adjustSearchContainer);
-
-document.getElementById("search-all").addEventListener("touchstart", function() {
-  setTimeout(() => {
-      this.focus();
-  }, 100);
-});
